ba738f1c1bd0032110e4fc9dcbcc1eacc8ef9f24
sanghakyi <sang105kr@gmail.com>
2023-06-13 09:28:49 +0900

    20230613-1

--- src/day3/Exam5.java
+++ src/day3/Exam5.java
@@ -1,4 +1,5 @@
 package day3;
+
 /*
   for문 연습
   별표 출력하기 5행 5열
@@ -10,26 +11,81 @@ package day3;
   *****     *****   *             *       *********
  */
 public class Exam5 {
-  public static void printStar1(){
+  public static void printStar1() {
     for (int i = 0; i < 5; i++) {
       for (int j = 0; j < 5; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void printStar2() {
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < i; j++) {
         System.out.printf("*");
       }
       System.out.println();
     }
   }
-  public static void printStar2(){
+
+  public static void printStar3() {
     for (int i = 0; i < 5; i++) {
-      for (int j = 0; j <= i; j++) {
+      for (int j = 0; j < 5 - i; j++) {
         System.out.printf("*");
       }
       System.out.println();
     }
   }
-  public static void printStar3(){
+
+  public static void printStar4(){
+
     for (int i = 0; i < 5; i++) {
-      for (int j = 0; j < 5-i ; j++) {
-        System.out.printf("*");
+      for (int j = 0; j < i  ; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 2*5-1; j > 2*i ; j--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void printStar5(){
+
+    for (int i = 0; i < 5; i++) {
+      for (int j = 5-1; j > i  ; j--) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < 2*i+1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+
+  public static void printStar6(){
+    int num = 10;
+    for (int i = 0; i < num; i++) {
+      for (int j = 0; j < i  ; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 2*num-1; j > 2*i ; j--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void printStar7(){
+    int num = 10;
+    for (int i = 0; i < num; i++) {
+      for (int j = num-1; j > i  ; j--) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < 2*i+1; j++) {
+        System.out.print("*");
       }
       System.out.println();
     }
@@ -38,5 +94,9 @@ public class Exam5 {
     printStar1();
     printStar2();
     printStar3();
+    printStar4();
+    printStar5();
+    printStar6();
+    printStar7();
   }
 }
