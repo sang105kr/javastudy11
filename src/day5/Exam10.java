501b85e8f79bc3c227dcb709d7f6c641605727af
sanghakyi <sang105kr@gmail.com>
2023-06-15 11:23:04 +0900

    20230615-3

---
+++ src/day5/Exam10.java
@@ -0,0 +1,18 @@
+package day5;
+
+public class Exam10 {
+  public static void main(String[] args) {
+    int[][][] arr = new int[2][][];
+    arr[0] = new int[2][];
+    arr[1] = new int[3][];
+
+    arr[0][0] = new int[]{1,2};
+    arr[0][1] = new int[]{4,5,6};
+
+    arr[1][0] = new int[]{6,7,4,7};
+    arr[1][1] = new int[]{3,4};
+    arr[1][2] = new int[]{5};
+
+
+  }
+}
--- src/day5/Problem2.java
+++ src/day5/Problem2.java
@@ -28,8 +28,85 @@ package day5;
 //    5번 선택시 아래 메세지 출력후 종료함
 //    프로그램 종료!!
 
+import java.util.Scanner;
+
 public class Problem2 {
+  public static void printDash(int cnt){
+    for (int i = 0; i < cnt; i++) {
+      System.out.print("-");
+    }
+    System.out.println();
+  }
   public static void main(String[] args) {
 
+    Scanner scanner = new Scanner(System.in);
+    boolean stop = false;
+    int[] score = null;
+
+    while (!stop) {
+      printDash(50);
+      System.out.println(" 1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료 ");
+      printDash(50);
+      System.out.print("선택>> ");
+      String selectedNum = scanner.nextLine();
+      switch (selectedNum) {
+        case "1":  //학생수
+          System.out.print("학생수> ");
+  //        String countOfStudent = scanner.nextLine();
+  //        int cnt = Integer.parseInt(countOfStudent);
+          int countOfStudent = Integer.parseInt(scanner.nextLine());
+          score = new int[countOfStudent];
+
+          break;
+        case "2":  //점수입력
+          if(score == null) {
+            System.out.println("학생수를 먼저 입력바랍니다");
+            continue;
+          }
+          for (int i = 0; i < score.length; i++) {
+            System.out.print("점수"+(i+1)+">> ");
+            int jumsu = Integer.parseInt(scanner.nextLine());
+            score[i] = jumsu;
+          }
+          break;
+        case "3":
+          if(score == null) {
+            System.out.println("학생수를 먼저 입력바랍니다");
+            continue;
+          }
+          for (int i = 0; i < score.length; i++) {
+            System.out.println("score["+i+"]=" + score[i]);
+          }
+          break;
+        case "4": // 분석
+          if(score == null) {
+            System.out.println("학생수를 먼저 입력바랍니다");
+            continue;
+          }
+          // 최대값
+          int max = score[0];
+          for (int i = 1; i < score.length; i++) {
+            if(max < score[i]){
+              max = score[i];
+            }
+          }
+          // 평균
+          int sum = 0;
+          for (int i = 0; i < score.length; i++) {
+            sum += score[i];
+          }
+          double avg = sum / (double)score.length;
+
+          System.out.println("최대값="+max);
+          System.out.println("평균="+avg);
+          break;
+        case "5": // 종료
+          stop = true;
+          break;
+        default:
+          System.out.println("1~5 번에서 고르세요");
+      }
+    }
+    System.out.println("프로그램 종료!");
   }
 }
