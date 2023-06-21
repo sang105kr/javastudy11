6aed4c72f3a4f34ec65d1d17fac0d4746231abed
sanghakyi <sang105kr@gmail.com>
2023-06-21 17:19:04 +0900

    20230621-8

--- src/day10/test/Account.java
+++ src/day10/test/Account.java
@@ -19,7 +19,7 @@ public class Account {
     this.accountOwnerName = accountOwnerName;
     accountNumber = changeAccountNumberToString(++accountNumberAccumulator);
   }
-  
+
   /**
    * 정수 계좌번호를 입력받아 문자열 계좌번호로 반환한다
    * ex) 12->"012"
@@ -41,20 +41,40 @@ public class Account {
 
   //  기능 :  입금, 출금, 조회
   //입금
-  void deposit() {
-
+  void deposit(int money) {
+    //1)1회 입금한도는 4만원을 초과할 수 없다.
+    if(money > 40_000){
+      System.out.println("1회 입금한도는 4만원을 초과할 수 없다.");
+      return;
+    }
+    //2)예치금액은 10만원 이하로 제한 한다.
+    if(this.balance + money > 100_000){
+      System.out.println("예치금액은 10만원 이하로 제한 한다.");
+      return;
+    }
+    this.balance += money;
   }
 
   //출금
-  void withdraw() {
-
+  void withdraw(int money) {
+    //1)1회 출금한도는 4만원을 초과할 수 없다.
+    if(money > 40_000){
+      System.out.println("1회 출금한도는 4만원을 초과할 수 없다.");
+      return;
+    }
+    //2)마이너스 잔고는 허용하지 않는다.
+    if (this.balance - money < 0) {
+      System.out.println("잔액이 부족합니다.");
+      return;
+    }
+    this.balance -= money;
   }
 
   //조회
   public String toString() {
-    return "예금주 : " + getAccountOwnerName() + "\t" +
-        "잔액 : " + getBalance() + "\t" +
-        "계좌번호 : " + getAccountNumber();
+    return "계좌번호 : " + getAccountNumber() + "\t" +
+        "예금주 : " + getAccountOwnerName() + "\t" +
+        "잔액 : " + getBalance();
   }
 
   public String getAccountOwnerName() {
--- src/day10/test/AccountMain.java
+++ src/day10/test/AccountMain.java
@@ -3,6 +3,23 @@ package day10.test;
 import java.util.Scanner;
 
 public class AccountMain {
+  /**
+   * 계좌번호와 일치하는 계좌를 참조하는 배열의 인덱스를 반환
+   * @param accountNumber 계좌번호
+   * @param accounts 계좌를 참조하는 배열
+   * @return 찾은 인덱스
+   */
+  private static int findIndexOfAccounts(String accountNumber, Account[] accounts){
+    int findedAccountIndex = -1;
+    for (int i = 0; i < accounts.length; i++) {
+      if (accounts[i] != null && accounts[i].getAccountNumber().equals(accountNumber)) {
+//              existAccount = true;
+        findedAccountIndex = i;
+        break;
+      }
+    }
+    return findedAccountIndex;
+  }
   public static void main(String[] args) {
 
     Account[] accounts = new Account[5];
@@ -24,19 +41,89 @@ public class AccountMain {
             }
           }
           break;
-        case "2":
+        case "2":  //폐지
+          System.out.print("계좌번호 >> ");
+          String accountNumber = scanner.nextLine();
+
+          // input : accounts, "찾고자하는 계좌번호"
+
+          //1) 찾고자하는 계좌가 있는지 체크
+          //boolean existAccount = false;
+          int findedAccountIndex = -1;
+          for (int i = 0; i < accounts.length; i++) {
+            if (accounts[i] != null && accounts[i].getAccountNumber().equals(accountNumber)) {
+//              existAccount = true;
+              findedAccountIndex = i;
+              break;
+            }
+          }
+
+          //2) 존재하면 null, 없으면 "계좌없음" 메세지
+          if(findedAccountIndex == -1){
+            System.out.println("계좌번호가 없습니다.");
+          }else{
+            System.out.printf("%s 계좌가 폐지 되었습니다.\n", accounts[findedAccountIndex].getAccountNumber());
+            accounts[findedAccountIndex] = null;
+          }
           break;
-        case "3":
+
+        case "3": // 입금
+          System.out.print("계좌번호 >> ");
+          accountNumber = scanner.nextLine();
+          findedAccountIndex = findIndexOfAccounts(accountNumber, accounts);
+          if (findedAccountIndex == -1) {
+            System.out.println("계좌번호가 없습니다.");
+            break;
+          }
+          System.out.print("입금액 >> ");
+//          String money = scanner.nextLine();
+          int money = Integer.parseInt(scanner.nextLine());
+          accounts[findedAccountIndex].deposit(money);
+          System.out.println(accounts[findedAccountIndex]);
           break;
-        case "4":
+        case "4": // 출금
+          System.out.print("계좌번호 >> ");
+          accountNumber = scanner.nextLine();
+          findedAccountIndex = findIndexOfAccounts(accountNumber, accounts);
+          if (findedAccountIndex == -1) {
+            System.out.println("계좌번호가 없습니다.");
+            break;
+          }
+          System.out.print("출금액 >> ");
+//          String money = scanner.nextLine();
+          money = Integer.parseInt(scanner.nextLine());
+          accounts[findedAccountIndex].withdraw(money);
+          System.out.println(accounts[findedAccountIndex]);
           break;
-        case "5":
+        case "5": //계좌조회(개별)
+          System.out.print("계좌번호 >> ");
+          accountNumber = scanner.nextLine();
+
+          //1) 찾고자하는 계좌가 있는지 체크
+          //boolean existAccount = false;
+          findedAccountIndex = -1;
+          for (int i = 0; i < accounts.length; i++) {
+            if (accounts[i] != null && accounts[i].getAccountNumber().equals(accountNumber)) {
+//              existAccount = true;
+              findedAccountIndex = i;
+              break;
+            }
+          }
+          //case1)
+          //System.out.println(accounts[findedAccountIndex]);
+
+          //case2)
+          System.out.printf("예금주명 : %s \t 잔액 : %d \t 계좌번호 : %s \n",
+              accounts[findedAccountIndex].getAccountOwnerName(),
+              accounts[findedAccountIndex].getBalance(),
+              accounts[findedAccountIndex].getAccountNumber());
+
           break;
         case "6": // 전체조회
           int usingCount = 0;
           for (int i = 0; i < accounts.length; i++) {
             if(accounts[i] != null) {
-              System.out.println(accounts[i].);
+              System.out.println(accounts[i]);
               usingCount++;
             }
           }
@@ -52,4 +139,5 @@ public class AccountMain {
     } while (!stop);
     System.out.println("프로그램 종료!");
   }
+
 }
