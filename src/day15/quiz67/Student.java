20f16822b8dd736f2169c6e25a16b57ea38359b0
sanghakyi <sang105kr@gmail.com>
2023-06-28 14:12:46 +0900

    20230628-5

---
+++ src/day15/quiz67/Student.java
@@ -0,0 +1,28 @@
+package day15.quiz67;
+
+public class Student {
+  private String name;        //학번
+  private String number;      //학번
+
+  public Student(String name, String number) {
+    // super();
+    this.name = name;
+    this.number = number;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getNumber() {
+    return number;
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+        "name='" + name + '\'' +
+        ", number='" + number + '\'' +
+        '}';
+  }
+}
---
+++ src/day15/quiz67/UnGraduate.java
@@ -0,0 +1,20 @@
+package day15.quiz67;
+
+public class UnGraduate extends Student{
+  private String clubName;    //동아리명
+  public UnGraduate(String name,String number) {
+    super(name,number);
+  }
+
+  public void setClubName(String clubName) {
+    this.clubName = clubName;
+  }
+
+  @Override
+  public String toString() {
+    return "UnGraduate{" +
+        "이름/학번=" + super.toString() +
+        "clubName='" + clubName + '\'' +
+        '}';
+  }
+}
