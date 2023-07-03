cefc149324ab11c5e3c2baefc4b458c47f0ffe66
sanghakyi <sang105kr@gmail.com>
2023-07-03 13:48:07 +0900

    '20230703-4'

--- src/com/kh/dic/Dictionary.java
+++ src/com/kh/dic/Dictionary.java
@@ -1,28 +1,72 @@
 package com.kh.dic;
 
-import com.sun.source.tree.Tree;
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
 
-public class Dictionary {
-  private TreeMap<String,String> dic;
+public class Dictionary extends TreeMap<String,String>{
+  private static final int WORD_CAPACITY = 5; // 단어 최대 저장 갯수
 
   public Dictionary() {
-    this.dic = new TreeMap<>();
+    super();
+    init();
   }
 
   //저장
-  public void save(String word,String meaning){
-    //중복체크
-    if(dic.containsKey(word)){
-      throw new RuntimeException("중복!");
+  public void save(String word, String meaning) {
+    String wordToLowerCase = word.toLowerCase();
+    //1) 단어수 초과
+    if(size() == WORD_CAPACITY){
+      throw new DictionaryException("최대 단어수 초과!");
+    };
+    //2) 단어 중복체크
+    if(containsKey(wordToLowerCase)){
+      throw new DictionaryException("이미 등록되었습니다!");
     }
-    dic.put(word,meaning);
+    put(wordToLowerCase, meaning);
   }
 
   //검색
+  public Map<String,String> findByWord(String word) {
+    String wordToLowerCase = word.toLowerCase();
+    Map<String, String> map = new HashMap<>();
+
+    //1)일치하는 단어를 검색
+    if(containsKey(wordToLowerCase)){
+      String meaning = get(wordToLowerCase);
+      map.put(wordToLowerCase,meaning);
+      return map;
+    }
+    //2)일치하는 단어가 없을경우. 즉, 범위검색
+    char firstLetter = wordToLowerCase.charAt(0);  //'s'
+    char nextLetter = (char)(wordToLowerCase.charAt(0) + 1); //'t'
+
+    map = subMap(wordToLowerCase, true, String.valueOf(nextLetter), false);
+
+    //3) 범위검색에도 포함되지 않으면
+    if(map.size() == 0) {
+      throw new DictionaryException("검색하고자 하는 단어가 없습니다.");
+    }
+    return map;
+  }
 
   //수정
+  public void update(String word, String meaning) {
 
+  }
+
+  //목록
+  public Map<String,String> listAll(){
+    return this;
+  }
+
+  //초기값
+  public void init(){
+    put("student", "학생");
+    put("teacher", "교사");
+    put("classroom", "교실");
+    put("smart", "현명한");
+    put("lunch", "점심");
+  }
 
 }
--- src/com/kh/dic/DictionaryMain.java
+++ src/com/kh/dic/DictionaryMain.java
@@ -1,28 +1,78 @@
 package com.kh.dic;
 
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class DictionaryMain {
   public static void main(String[] args) {
-     Dictionary dic = new Dictionary();
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("메뉴:1.저장 2.검색 3.수정 4.삭제 5.목록 6.색인 7.통계 8.종료");
-    boolean stop = false;
-    do {
-      switch (1) {
-        case 1:
-          break;
-        case 2:
-          break;
-        case 3:
-          break;
-        case 4:
-          break;
-        case 5:
-          break;
 
+    Scanner scanner = new Scanner(System.in);
+    Dictionary dic = new Dictionary();
+    boolean stop = false;
+
+    do {
+      System.out.println("1.저장 2.검색 3.수정 4.삭제 5.목록 6.색인 7.통계 8.종료 9.초기화 10.비우기");
+      System.out.print("선택 >> ");
+      String menu = scanner.nextLine();
+      String word = "";       //단어
+      String meaning = "";    //의미
+      Map<String, String> findedWords = null; // 검색된 단어목록;
+      switch (menu) {
+        case "1": //저장
+          System.out.print("단어 입력 : ");
+          word = scanner.nextLine();
+          System.out.print("의미 입력 : ");
+          meaning = scanner.nextLine();
+          try {
+            dic.save(word, meaning);
+          } catch (DictionaryException e) {
+            System.out.println(e.getMessage());
+          }
+          break;
+        case "2": //검색
+          System.out.print("단어 입력 : ");
+          word = scanner.nextLine();
+          findedWords = dic.findByWord(word);
+          //case1) keyset
+          Set<String> words = findedWords.keySet();
+          for (String key : words) {
+            System.out.printf("%s : %s\n",key,findedWords.get(key));
+          }
+          //case2) entry
+//          Set<Map.Entry<String, String>> entries = findedWords.entrySet();
+//          for (Map.Entry<String, String> entry : entries) {
+//            System.out.printf("%s : %s\n",entry.getKey(),entry.getValue());
+//          }
+
+          break;
+        case "3":
+          break;
+        case "4":
+          break;
+        case "5": //목록
+          Map<String,String> map = dic.listAll();
+          for(String key : map.keySet()){
+            System.out.printf("%s : %s\n",key,map.get(key));
+          }
+          break;
+        case "6":
+          break;
+        case "7":
+          break;
+        case "8":
+          stop = true;
+          break;
+        case "9": //초기화
+          dic.init();
+          break;
+        case "10": //비우기
+          dic.clear();
+          break;
+        default:
+          System.out.println("잘못된 메뉴 선택!(1~8)");
       }
     } while (!stop);
-
+    System.out.println("영어 단어장을 종료합니다.");
   }
 }
